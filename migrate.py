f97d1379f799edb8988c223e198b10557716926c
Krishna <krishnayad997@gmail.com>
2020-05-04 19:42:21 +0800

    Deploying now

--- migrate.py
+++ migrate.py
@@ -1,6 +1,20 @@
 import peeweedbevolve
 from database import db
 from bid import Bid
+from models import *
+import os
 
-if __name__ == '__main__':
-   db.evolve(ignore_tables={'base_model'})
+os.environ['MIGRATION'] = '1'
+
+if not os.getenv('FLASK_ENV') == 'production':
+    print("Loading environment variables from .env")
+    from dotenv import load_dotenv
+    load_dotenv()
+
+
+print("Running Migration")
+if os.getenv('FLASK_ENV') == 'production':
+    db.evolve(ignore_tables={'base_model'}, interactive=False)
+else:
+    db.evolve(ignore_tables={'base_model'})
+print("Finish Migration")
